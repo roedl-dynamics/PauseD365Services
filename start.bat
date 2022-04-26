19be6f13c37b958bd8f30c7b5ccdca6e709f0007
Henryk Ohls <henrykohls@gmx.de>
2022-04-26 10:24:36 +0200

    Added scripts
    Stop and star scripts.

---
+++ start.bat
@@ -0,0 +1,5 @@
+NET START "MR2012ProcessService"
+NET START "DynamicsAxBatch"
+NET START "Microsoft.Dynamics.AX.Framework.Tools.DMF.SSISHelperService.exe"
+NET START "W3SVC"
+PAUSE
---
+++ stop.bat
@@ -0,0 +1,5 @@
+NET STOP "MR2012ProcessService"
+NET STOP "DynamicsAxBatch"
+NET STOP "Microsoft.Dynamics.AX.Framework.Tools.DMF.SSISHelperService.exe"
+NET STOP "W3SVC"
+PAUSE
